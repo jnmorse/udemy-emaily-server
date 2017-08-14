88b790e9b6a27494e4f3d0f66df94c3892ebaf61
Joseph Morse <tamed.lionheart@gmail.com>
2017-08-14 12:14:55 -0400

    Moved Routes and Passport
    Moved routes and passport configuration to external files.

--- index.js
+++ index.js
@@ -2,30 +2,11 @@ const PORT = process.env.PORT || 5000;
 
 // Require Express
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 // Set App Variable
 const app = express();
 
-passport.use(new GoogleStrategy(
-  Object.assign(keys.google, { callbackURL: '/auth/google/callback'}),
-  (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
-  }
-));
-
-// app.get('/', (req, res) => {});
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 app.listen(PORT);
