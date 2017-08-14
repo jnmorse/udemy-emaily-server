19c527840a44d8da9d9804ce6f776154e50c6c29
Joseph Morse <tamed.lionheart@gmail.com>
2017-08-14 11:54:59 -0400

    Start For Google Authenitcation
    Setup the basic configureation for google oauth

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+const PORT = process.env.PORT || 5000;
+
 // Require Express
 const express = require('express');
-
-const PORT = process.env.PORT || 5000;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 // Set App Variable
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy(
+  Object.assign(keys.google, { callbackURL: '/auth/google/callback'}),
+  (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
+  }
+));
+
+// app.get('/', (req, res) => {});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT);
