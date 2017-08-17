5d182c9d75162abb3d7c49d4ed589bfd17e2c717
Joseph Morse <tamed.lionheart@gmail.com>
2017-08-17 14:55:41 -0400

    Fixed Some Typos

--- config/prod.js
+++ config/prod.js
@@ -1,13 +1,13 @@
 module.exports = {
   google: {
-    ClientID: process.env.GOOGLE_CLIENT_ID,
-    ClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
   },
-  mongo: {
+  mlab: {
     uri: process.env.MONGO_URI,
     options: {
       user: process.env.MONGO_USER,
-      password: process.env.MONGO_PASS
+      pass: process.env.MONGO_PASS
     }
   },
   cookieKey: process.env.COOKIE_KEY
