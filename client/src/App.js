4c25013ada996172cab5c1014cb83e4f8fe887c6
Joseph Morse <tamed.lionheart@gmail.com>
2017-08-22 00:40:51 -0400

    Completed up to stop 85

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux'
+import * as actions from './actions';
 
+import Header from './components/Header';
+import Landing from './components/Landing';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Route exact path="/" component={ Landing } />
-      <Route exact path="/surveys" component={ Dashboard } />
-      <Route path="/surveys/new" component={ SurveyNew } />
-    </div>
-  </BrowserRouter>
-);
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  
+  render() {
+    return(
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={ Landing } />
+          <Route exact path="/surveys" component={ Dashboard } />
+          <Route path="/surveys/new" component={ SurveyNew } />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
-export default App;
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return(
+          <a href="/auth/google">{'Login With Google'}</a>
+        );
+      default:
+        return(
+          <ul>
+            <li>
+              <Link to="/surveys">{'Dashboard'}</Link>
+            </li>
+            
+            <li>
+              <a href="/api/logout">{'Logout'}</a>
+            </li>
+          </ul>
+        );
+    }
+  }
+  
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="brand-logo">
+          <Link to="/" className="left brand-logo">
             E-Maily
-          </a>
+          </Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="/auth/google">Login With Google</a>
+              {this.renderContent()}
             </li>
           </ul>
         </div>
@@ -19,4 +44,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
