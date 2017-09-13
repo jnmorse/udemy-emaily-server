0f9707117d9589ac9f6615b367da9d28abd7fc84
Joseph Morse <tamed.lionheart@gmail.com>
2017-09-12 20:34:38 -0400

    Removed Const and Var name
    Fell back into what I was doing before and added const to the
    fetch_surveys action. Have now removed.

--- client/src/actions/fetch-surveys.js
+++ client/src/actions/fetch-surveys.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { FETCH_SURVEYS } from './types';
 
-export const fetchSurveys = () => async dispatch => {
+export default () => async dispatch => {
   const res = await axios.get('/api/surveys');
   
   dispatch({ type: FETCH_SURVEYS, payload: res.data });
